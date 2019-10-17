39503ce52bcb3d112a1a27238b889b06a4624d63
Anton Borovyk <54375146+vorlodka@users.noreply.github.com>
2019-10-17 11:16:50 +0300

    Update Лаба 3.py

--- Лаба 3.py
+++ Лаба 3.py
@@ -1,10 +1,10 @@
 import math
 
 number = math.sin(0.56) #Перший член послідовності
-counter = 1
+counter = 1 #Значення n
 summ = 0    #Cума послідовності n-ї кількості number
 while number > 0.000001:
-    summ = summ + number
+    summ = summ + number #Доплюсовуємо до загальної суми
     print("Доданок %s: %8.6f" % (counter, number))
     counter = counter + 1  #Номер цикла
     number = 1 / (counter ** 2) * math.sin(0.56) #Розрахунок значення наступного члена суми
